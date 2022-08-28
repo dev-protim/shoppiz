d6f5c3a54504b5f9b60d63193e86eaa4f0d73db5
dev-protim <roypranto445@gmail.com>
2022-08-29 02:17:44 +0600

    Add carousel and header.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // Hero carousel starts
+    $('.hero-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        items: 1,
+        autoplay: true,
+        smartSpeed: 500
+    })
+    // Hero carousel ends
+});
