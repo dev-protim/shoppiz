2930e979a91a092cde0c1f68a9a7f1b4a43493ea
dev-protim <roypranto445@gmail.com>
2022-09-13 01:06:56 +0600

    Add isotope.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,40 @@ jQuery(function ($) {
         smartSpeed: 500
     })
     // Hero carousel ends
+
+    // Masontry starts
+    var $grid = $('.isotope-grid').isotope({
+        // options
+      });
+    $('.isotope-group').on( 'click', 'li', function() {
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+    // Masontry ends
+
+    // Collection carousel starts
+    $('.collection-carousel').owlCarousel({
+        stagePadding: 50,
+        loop: true,
+        margin: 20,
+        nav: false,
+        dots: false,
+        items: 3,
+        // autoplay: false,
+        // smartSpeed: 500,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    })
+    // Collection carousel ends
 });
